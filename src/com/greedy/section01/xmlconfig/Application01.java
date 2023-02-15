5314b066b2e2a67745852bf5869fdefb6ca0ae74
tjoeun <tjoeun@DESKTOP-PJLQ947>
2023-02-15 18:12:40 +0900

    공부

---
+++ src/com/greedy/section01/xmlconfig/Application01.java
@@ -0,0 +1,14 @@
+package com.greedy.section01.xmlconfig;
+
+import static com.greedy.section01.xmlconfig.Template.getSqlSession;
+public class Application01 {
+
+	public static void main(String[] args) {
+
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		
+	}
+
+}
